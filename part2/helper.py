b299bb497b9eac03a79bc8f641d72de2be394790
remcdaniel <remcdaniel@uiowa.edu>
2020-12-18 02:36:19 -0600

    ope

--- part2/helper.py
+++ part2/helper.py
@@ -91,3 +91,133 @@ def createOptimizers(adamNets, sgdNets, learningRates):
         sgdOptimizers.append(optim.SGD(sgdNets[i].parameters(), learningRates[i]))
 
     return adamOptimizers, sgdOptimizers
+
+def runCNN_noStop(trainloader, device, optimizer, net, criterion, validloader, PATH, fileName, epochNum):
+    trainLoss = []
+    validationLoss = []
+
+    for _ in range(epochNum):
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data[0].to(device), data[1].to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+        epoch_loss = running_loss / (i+1)
+        trainLoss.append(epoch_loss)
+        
+        with torch.no_grad(): 
+            running_loss = 0.0
+            for i, data in enumerate(validloader, 0):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data[0].to(device), data[1].to(device)
+
+                # forward 
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                running_loss += loss.item()
+
+            epoch_loss = running_loss / (i+1)
+            validationLoss.append(epoch_loss)
+
+            torch.save(net.state_dict(), OO.path.join(PATH, "{}.pth".format(fileName)))
+
+    return trainLoss, validationLoss
+
+def runCNN_earlyStop(trainloader, device, optimizer, net, criterion, validloader, PATH, fileName, epochNum):
+    best_loss = np.float('inf')
+    trainLoss = []
+    validationLoss = []
+    best_epoch = 0
+    range_epochs = 5
+
+    for epoch in range(epochNum):
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data[0].to(device), data[1].to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+        epoch_loss = running_loss / (i+1)
+        trainLoss.append(epoch_loss)
+        
+        with torch.no_grad(): 
+            running_loss = 0.0
+            for i, data in enumerate(validloader, 0):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data[0].to(device), data[1].to(device)
+
+                # forward 
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                running_loss += loss.item()
+
+            epoch_loss = running_loss / (i+1)
+            validationLoss.append(epoch_loss)
+
+            # save the best model based on validation loss
+            if epoch_loss < best_loss:
+                torch.save(net.state_dict(), OO.path.join(PATH, "{}.pth".format(fileName)))
+                best_loss = epoch_loss
+                best_epoch = epoch
+
+            # stop early if it has been several epochs since last best
+            if (epoch - best_epoch) > range_epochs:
+                break
+
+    return trainLoss[0:best_epoch], validationLoss[0:best_epoch]
+
+def testCNNwithConfusionMatrix(testloader, net, device, class_correct, class_total):
+    correct, total = 0, 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images.to(device))
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels.to(device)).sum().item()
+            c = (predicted == labels.to(device))
+            for i in range(len(c)):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+    return correct, total, class_correct, class_total
+
+def manyAccuracies(accuracy, testNet, kind, optimizers, loadNet, testLoader, learningRates, PATH, numTests, device):
+    for i in range(len(learningRates)):    
+        temp = []
+        newPath = PATH + "/{}_lr_{}".format(kind, optimizers[i].defaults['lr'])
+        for j in range(numTests):
+            fileName = "{}_epoch_{}_lr_{}.pth".format(kind, j, learningRates[i])
+            testNet.load_state_dict(torch.load(OO.path.join(newPath, fileName)))
+            correct, total = testCNN(testLoader, testNet, device)
+            temp.append(100 * correct / total)
+        accuracy.append(temp)
+
+def eachAccuracy(testNet, numTests, PATH, loadNet, testLoader, device):
+    accuracy = []
+    for j in range(numTests):
+        testNet.load(PATH, "{}_epoch_{}.pth".format(loadNet, j))
+        correct, total = testCNN(testLoader, testNet, device)
+        accuracy.append(100 * correct / total)
+    return accuracy
--- part2/net.py
+++ part2/net.py
@@ -48,17 +48,16 @@ class ReluNet(nn.Module):
         x = self.fc3(x)
         return x
 
-# define your CNN here, e.g. activation function: F.sigmoid or F.relu
 class ReluNetWide(nn.Module):
     def __init__(self):
-        super(ReluNet, self).__init__()
+        super(ReluNetWide, self).__init__()
         self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
         self.fc1 = nn.Linear(32 * 7 * 7, 512)
-        self.fc2 = nn.Linear(512, 128)
-        self.fc3 = nn.Linear(128, 10)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
         self.act = nn.ReLU()
 
     def forward(self, x):
@@ -71,25 +70,28 @@ class ReluNetWide(nn.Module):
         x = self.fc3(x)
         return x
 
-# define your CNN here, e.g. activation function: F.sigmoid or F.relu
 class ReluNetDeep(nn.Module):
     def __init__(self):
-        super(ReluNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        super(ReluNetDeep, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
         self.fc1 = nn.Linear(32 * 7 * 7, 512)
-        self.fc2 = nn.Linear(512, 128)
-        self.fc3 = nn.Linear(128, 10)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 10)
         self.act = nn.ReLU()
 
     def forward(self, x):
-        x = self.pool(self.act(self.conv1(x)))
+        x = self.act(self.conv1(x))
         x = self.pool(self.act(self.conv2(x)))
-        x = self.act(self.conv3(x))
+        x = self.pool(self.act(self.conv3(x)))
+        x = self.act(self.conv4(x))
         x = x.view(-1, 32 * 7 * 7)
         x = self.act(self.fc1(x))
         x = self.act(self.fc2(x))
-        x = self.fc3(x)
+        x = self.act(self.fc3(x))
+        x = self.fc4(x)
         return x
